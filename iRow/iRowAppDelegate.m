072f81ac7ae9677ba349f4885cbcf6a3e431a313
David A. van Leeuwen <david.vanleeuwen@gmail.com>
2011-09-23 15:30:20 +0200

    Even more subtle location manager updating: Don't update in inactive state if not started.

--- iRow/iRowAppDelegate.m
+++ iRow/iRowAppDelegate.m
@@ -38,6 +38,8 @@
      Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
      Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
      */
+    if (!ergometerViewController.started) 
+        [ergometerViewController.track.locationManager stopUpdatingLocation];
 }
 
 - (void)applicationDidEnterBackground:(UIApplication *)application
@@ -46,7 +48,7 @@
      Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later. 
      If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
      */
-    [ergometerViewController.locationManager stopUpdatingLocation];
+    [ergometerViewController.track.locationManager stopUpdatingLocation];
 }
 
 - (void)applicationWillEnterForeground:(UIApplication *)application
@@ -54,7 +56,8 @@
     /*
      Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
      */
-    [ergometerViewController.locationManager startUpdatingLocation];
+    if (ergometerViewController.started) 
+        [ergometerViewController.track.locationManager startUpdatingLocation];
 }
 
 - (void)applicationDidBecomeActive:(UIApplication *)application
@@ -62,6 +65,7 @@
     /*
      Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
      */
+    [ergometerViewController.track.locationManager startUpdatingLocation];
 }
 
 - (void)applicationWillTerminate:(UIApplication *)application
