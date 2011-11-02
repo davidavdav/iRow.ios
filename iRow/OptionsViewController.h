5dad304bd25f2ff6a8818cb4d934a227c8f1f1dd
David A. van Leeuwen <david.vanleeuwen@gmail.com>
2011-11-02 23:59:14 +0100

    new tableview navigationcontroller instead of pure help file.

---
+++ iRow/OptionsViewController.h
@@ -0,0 +1,15 @@
+//
+//  SettingsViewController.h
+//  iRow
+//
+//  Created by David van Leeuwen on 02-11-11.
+//  Copyright (c) 2011 strApps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface OptionsViewController : UITableViewController {
+    NSArray * sectionTitles;
+}
+
+@end
