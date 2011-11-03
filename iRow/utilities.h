812b6a713e562272a86fe5197c822e535b9acfc2
David A. van Leeuwen <david.vanleeuwen@gmail.com>
2011-11-03 21:25:35 +0100

    moved general utilities form Settings to this file

---
+++ iRow/utilities.h
@@ -0,0 +1,16 @@
+//
+//  utilities.h
+//  iRow
+//
+//  Created by David van Leeuwen on 03-11-11.
+//  Copyright (c) 2011 strApps. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <CoreLocation/CoreLocation.h>
+
+// some utility functions
+NSString * dispLength(CLLocationDistance l);
+NSString * dispLengthOnly(CLLocationDistance l);
+NSString * dispLengthUnit(CLLocationDistance l);
+
