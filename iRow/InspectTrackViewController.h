28177a62a18fe2be3c8eebbbb5f32642ca1d308a
David A. van Leeuwen <david.vanleeuwen@gmail.com>
2011-11-16 18:04:57 +0100

    - herannotation protocol: reports on change of boat position in track view  - implement change of position by dragging boat indicator  - implemented slider to interpolate position on track  - show track current speeds - removed gesturerecognizer: does not work for me.  - use cameralight as boat position

--- iRow/InspectTrackViewController.h
+++ iRow/InspectTrackViewController.h
@@ -11,12 +11,33 @@
 #import "Track.h"
 #import "TrackData.h"
 
-@interface InspectTrackViewController : UIViewController <MKMapViewDelegate> {
+@protocol HereAnnotationViewDelegte <NSObject>
+
+-(void)hereAnnotationMoved:(CLLocationCoordinate2D)coordinate index:(NSInteger)index;
+
+@end
+
+@interface HereAnnotation: MKPointAnnotation 
+@end
+
+@interface HereAnnotationView : MKAnnotationView {
+    MKMapView * mapView;
+    id <HereAnnotationViewDelegte> delegate;
+}
+@property (nonatomic, strong) MKMapView * mapView;
+@property (nonatomic, strong) id <HereAnnotationViewDelegte> delegate;
+@end
+
+@interface InspectTrackViewController : UIViewController <MKMapViewDelegate, HereAnnotationViewDelegte> {
     MKMapView * mapView;
     UIView * infoView;
     Track * track;
     TrackData * trackData;
     MKPolyline * polyLine;
+    UISlider * slider;
+    UILabel * timeLabel, * distLabel, * speedLabel;
+    HereAnnotation * here;
+    
 }
 
 @property (nonatomic, strong) Track * track;
--- iRow/InspectTrackViewController.m
+++ iRow/InspectTrackViewController.m
@@ -7,6 +7,45 @@
 //
 
 #import "InspectTrackViewController.h"
+#import "utilities.h"
+#import "Settings.h"
+
+@implementation HereAnnotation
+@end
+
+@implementation HereAnnotationView
+
+@synthesize mapView;
+@synthesize delegate;
+
+-(void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event {
+    for (UITouch * t in event.allTouches) {
+//        NSLog(@"began %@", t);
+    }
+}
+
+-(void)touchesMoved:(NSSet *)touches withEvent:(UIEvent *)event {
+    for (UITouch * t in event.allTouches) {
+        CLLocationCoordinate2D c = [mapView convertPoint:[t locationInView:self] toCoordinateFromView:self];
+//        NSLog(@"moved %f %f", c.longitude, c.latitude);
+        MKMapPoint mp = MKMapPointForCoordinate(c);
+        MKPolyline * pl = [mapView.overlays objectAtIndex:0];
+        CLLocationDistance min = 1e9;
+        int mi = -1;
+        for (int i=0; i<pl.pointCount; i++) {
+            CLLocationDistance d = MKMetersBetweenMapPoints(mp, pl.points[i]);
+            if (d<min) {
+                min=d;
+                mi=i;
+            }
+        }
+        self.annotation.coordinate = MKCoordinateForMapPoint(pl.points[mi]);
+        if (delegate) [delegate hereAnnotationMoved:self.annotation.coordinate index:mi];
+    }    
+}
+
+@end
+
 
 @implementation InspectTrackViewController
 
@@ -40,6 +79,28 @@
     // Release any cached data, images, etc that aren't in use.
 }
 
+-(void)sliderChanged:(id)sender {
+    double dist = slider.value * trackData.totalDistance;
+    CLLocation * l = [trackData interpolate:dist];
+    here.coordinate = l.coordinate;
+    timeLabel.text = [NSString stringWithFormat:@"%@ m:s",hms([l.timestamp timeIntervalSinceDate:[(CLLocation*)[trackData.locations objectAtIndex:0] timestamp]])];
+    distLabel.text = dispLength(dist);
+    speedLabel.text = [NSString stringWithFormat:@"%@ %@",dispSpeed(l.speed, Settings.sharedInstance.speedUnit),dispSpeedUnit(Settings.sharedInstance.speedUnit)];
+    return;
+}
+
+#pragma mark HereAnnotationViewDelegate
+-(void)hereAnnotationMoved:(CLLocationCoordinate2D)coordinate index:(NSInteger)index {
+    CLLocation * l = [trackData.locations objectAtIndex:index];
+    timeLabel.text = [NSString stringWithFormat:@"%@ m:s",hms([l.timestamp timeIntervalSinceDate:[(CLLocation*)[trackData.locations objectAtIndex:0] timestamp]])];
+    CLLocationDistance d = [[trackData.cumDist objectAtIndex:index] floatValue];
+    distLabel.text = dispLength(d);
+    speedLabel.text = [NSString stringWithFormat:@"%@ %@",dispSpeed(l.speed, Settings.sharedInstance.speedUnit),dispSpeedUnit(Settings.sharedInstance.speedUnit)];
+    slider.value = d/trackData.totalDistance;
+    return;   
+}
+
+
 #pragma mark - View lifecycle
 
 // Implement loadView to create a view hierarchy programmatically, without using a nib.
@@ -47,17 +108,41 @@
 {
     [super loadView];
     CGRect frame = self.view.bounds;
-    frame.size.height = frame.size.height - self.tabBarController.tabBar.bounds.size.height;
+    CGFloat h = frame.size.height - self.tabBarController.tabBar.bounds.size.height - self.navigationController.navigationBar.bounds.size.height;
+    CGFloat w = frame.size.width;
+    CGFloat hSlider = 40, hLabel = 40;
+    frame.size.height = h - hLabel - hSlider;
+    frame.origin.y = hLabel;
     mapView = [[MKMapView alloc] initWithFrame:frame];
     [self.view addSubview:mapView];
     mapView.delegate = self;
+    UIView * panel = [[UIView alloc] initWithFrame:CGRectMake(0, 0, w, hSlider)];
+    [self.view addSubview:panel];
+    slider = [[UISlider alloc] initWithFrame:CGRectMake(10, h - hSlider, w-20, hSlider)];
+    [self.view addSubview:slider];
+    timeLabel = [[UILabel alloc] initWithFrame:CGRectMake(10, 10, 100, 20)];
+    distLabel = [[UILabel alloc] initWithFrame:CGRectMake(120, 10, 100, 20)];
+    speedLabel = [[UILabel alloc] initWithFrame:CGRectMake(w-100-10, 10, 100, 20)];
+    for (UILabel * l in [NSArray arrayWithObjects:timeLabel,distLabel,speedLabel,nil] ) {
+        l.textColor = UIColor.whiteColor;
+        l.backgroundColor = UIColor.blackColor;
+        l.font = [UIFont systemFontOfSize:20];
+    }
+    [panel addSubview:timeLabel];
+    [panel addSubview:distLabel];
+    [panel addSubview:speedLabel];
     if (track != nil) {
-        self.title = track.name;
+        self.title = (track.name == nil || [track.name isEqualToString:@""]) ? @"Track details" : [NSString stringWithFormat:@"Details for %@",track.name];
         trackData = [NSKeyedUnarchiver unarchiveObjectWithData:track.track];
         polyLine = trackData.polyLine;
         [mapView addOverlay:polyLine];
         [mapView setRegion:trackData.region];
         [mapView addAnnotations:trackData.pins];
+        // slider 
+        [slider addTarget:self action:@selector(sliderChanged:) forControlEvents:UIControlEventValueChanged];
+        here = [[HereAnnotation alloc] init];
+        [self sliderChanged:self];    
+        [mapView addAnnotation:here];
     }
 }
 
@@ -90,8 +175,11 @@
         MKPolylineView * trackView = [[MKPolylineView alloc] initWithPolyline:overlay];
         trackView.strokeColor = [UIColor purpleColor];
         trackView.lineWidth = 5;
-        UIGestureRecognizer * rec = [[UIGestureRecognizer alloc] initWithTarget:self action:@selector(trackTouched:)];
-        [trackView addGestureRecognizer:rec];
+/* 
+  UIGestureRecognizer * rec = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(trackTouched:)];
+        [mapView addGestureRecognizer:rec];
+        NSLog(@"trackview %f %f", trackView.frame.origin.x, trackView.frame.origin.y);
+ */ 
         return trackView;
     }
     return nil;
@@ -103,6 +191,15 @@
 {  
     // the home location
     if ([annotation isKindOfClass:[MKUserLocation class]]) return nil;
+    if ([annotation isKindOfClass:[HereAnnotation class]]) {
+        HereAnnotationView * image = (HereAnnotationView*)[mv dequeueReusableAnnotationViewWithIdentifier:@"loc"];
+        if (image == nil) image = [[HereAnnotationView alloc] initWithAnnotation:annotation reuseIdentifier:@"loc"];
+        else image.annotation = annotation;
+        image.mapView = mapView;
+        image.delegate = self;
+        image.image = [UIImage imageNamed:@"PLCameraButtonRecordOn"];
+        return image;
+    }
     MKPinAnnotationView *pin = (MKPinAnnotationView *) [mv dequeueReusableAnnotationViewWithIdentifier: @"pin"];
     if (pin == nil)
     {
@@ -110,15 +207,22 @@
     } else {
         pin.annotation = annotation;
     }
-    pin.pinColor = MKPinAnnotationColorPurple;
+    if ([annotation isKindOfClass:[HereAnnotation class]])
+        pin.pinColor = MKPinAnnotationColorGreen;
+    else 
+        pin.pinColor = MKPinAnnotationColorPurple;
     pin.animatesDrop = NO;
-    pin.canShowCallout = YES;
+    pin.canShowCallout = NO;
     pin.draggable = NO;
     return pin;
 }
 
--(void)trackTouched:(id)sender {
-    NSLog(@"Wow, I feel touched!");
+/*
+-(void)trackTouched:(UIGestureRecognizer*)sender {
+    CGPoint p = [sender locationInView:mapView];
+    NSLog(@"Location %f %f", p.x, p.y);
 }
+
+ */
 
 @end
