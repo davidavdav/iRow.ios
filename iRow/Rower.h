326897115bbf5653fd15216734f8fa40e2c085ea
David A. van Leeuwen <david.vanleeuwen@gmail.com>
2011-11-18 00:21:01 +0100

    added steering reverse relation

--- iRow/Rower.h
+++ iRow/Rower.h
@@ -2,33 +2,35 @@
 //  Rower.h
 //  iRow
 //
-//  Created by David van Leeuwen on 06-11-11.
+//  Created by David van Leeuwen on 17-11-11.
 //  Copyright (c) 2011 strApps. All rights reserved.
 //
 
 #import <Foundation/Foundation.h>
 #import <CoreData/CoreData.h>
 
+@class Course, Track;
 
 @interface Rower : NSManagedObject
 
 @property (nonatomic, retain) NSDate * birthDate;
 @property (nonatomic, retain) NSString * email;
+@property (nonatomic, retain) NSNumber * mass;
 @property (nonatomic, retain) NSString * name;
 @property (nonatomic, retain) NSNumber * power;
-@property (nonatomic, retain) NSNumber * mass;
-@property (nonatomic, retain) NSSet *tracks;
 @property (nonatomic, retain) NSSet *courses;
+@property (nonatomic, retain) NSSet *tracks;
+@property (nonatomic, retain) Track *stearing;
 @end
 
 @interface Rower (CoreDataGeneratedAccessors)
 
-- (void)addTracksObject:(NSManagedObject *)value;
-- (void)removeTracksObject:(NSManagedObject *)value;
-- (void)addTracks:(NSSet *)values;
-- (void)removeTracks:(NSSet *)values;
-- (void)addCoursesObject:(NSManagedObject *)value;
-- (void)removeCoursesObject:(NSManagedObject *)value;
+- (void)addCoursesObject:(Course *)value;
+- (void)removeCoursesObject:(Course *)value;
 - (void)addCourses:(NSSet *)values;
 - (void)removeCourses:(NSSet *)values;
+- (void)addTracksObject:(Track *)value;
+- (void)removeTracksObject:(Track *)value;
+- (void)addTracks:(NSSet *)values;
+- (void)removeTracks:(NSSet *)values;
 @end
--- iRow/Rower.m
+++ iRow/Rower.m
@@ -2,21 +2,24 @@
 //  Rower.m
 //  iRow
 //
-//  Created by David van Leeuwen on 06-11-11.
+//  Created by David van Leeuwen on 17-11-11.
 //  Copyright (c) 2011 strApps. All rights reserved.
 //
 
 #import "Rower.h"
+#import "Course.h"
+#import "Track.h"
 
 
 @implementation Rower
 
 @dynamic birthDate;
 @dynamic email;
+@dynamic mass;
 @dynamic name;
 @dynamic power;
-@dynamic mass;
-@dynamic tracks;
 @dynamic courses;
+@dynamic tracks;
+@dynamic stearing;
 
 @end
