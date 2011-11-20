1d854784e4bf88496f454609fa00d136da36393d
David A. van Leeuwen <david.vanleeuwen@gmail.com>
2011-11-20 01:07:21 +0100

    use utilities

--- iRow/Stroke.m
+++ iRow/Stroke.m
@@ -7,12 +7,9 @@
 //
 
 #import "Stroke.h"
+#import "utilities.h"
 
 #define kMaxSamples (1000)
-
-#define kMinStrokeSens (0.05)
-#define kMaxStrokeSens (0.5)
-#define kLogSensRange (2.0)
 
 @implementation Stroke
 
@@ -82,7 +79,7 @@
     strokes = 0;
 }
 -(void)setSensitivity:(float)logSens {
-    threshold = kMaxStrokeSens * pow(kMinStrokeSens/kMaxStrokeSens,logSens/kLogSensRange);
+    threshold = strokeSensitivity(logSens);
 }
 
 
