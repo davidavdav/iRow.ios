fc866aa624bf4cc1b79bf9ad11f0a7004f4a13a2
David A. van Leeuwen <david.vanleeuwen@gmail.com>
2012-08-26 22:46:28 +0200

    - defined constants  - renamed size blocksize;  - hwperiod, lowp filters,  - stroke direction  - change device motion to accellerometer  - code for 100Hz HW sampling  - downsampling  - automatic orientation detection  -

--- iRow/Stroke.h
+++ iRow/Stroke.h
@@ -13,20 +13,33 @@
 #import "Filter.h"
 #import "Vector.h"
 #import "Matrix.h"
+#import "VectorMA.h"
+
+#define kBlockSizeSeconds (100)
+#define kBlockArrayStartsize (100)
+#define kHardwareSamplingRate (100.0)
+// #define kMaxSamples (1000)
+
 
 @protocol StrokeDelegate <NSObject>
 -(void)stroke:(id)sender;
 @end
 
 @interface Stroke : NSObject <NSCoding> {
+    BOOL live, downsampling, autoOrientation;
     CMMotionManager * motionManager;
     NSTimer * motionTimer;
-    int size, N, ptr;
+    int blocksize, N, ptr; // accelleration storage size, total recorded, current point. 
     Matrix * acc; // current period of data
+    int covbuffersize; 
+    Vector * direction; // current accelleration direction
+    VectorMA * ma;
     NSMutableArray * strokeData; // all stroke data
-    CFTimeInterval period, duration;
+    CFTimeInterval period, hwperiod, duration; // effective sampling period, hardware sampling period, covariance measurement duration
+    double hardwareSamplingRate;
     int sign;
-    Filter * bpx, * bpy;
+    Filter * bpx, * bpy; // bandpass filters for stoke data
+    Filter * lowp[3]; // lowpass filter for accelleration data
     int strokes;
     id<StrokeDelegate> delegate;
     float threshold;
@@ -36,6 +49,7 @@
 
 @property (strong, nonatomic) CMMotionManager * motionManager;
 @property (strong, nonatomic) Matrix * acc;
+@property (strong, nonatomic) Vector * direction;
 @property (readonly) int strokes;
 @property (readonly) CFTimeInterval period;
 @property (strong, nonatomic) id<StrokeDelegate> delegate;
@@ -49,6 +63,7 @@
 -(void)setSensitivity:(float)logSens;
 -(void)startRecording;
 -(void)stopRecording;
+-(void)hundredHzSampling:(BOOL)yes;
 
 -(Vector*)accData:(NSInteger)dim;
 
--- iRow/Stroke.m
+++ iRow/Stroke.m
@@ -8,13 +8,15 @@
 
 #import "Stroke.h"
 #import "utilities.h"
+#import "Matrix+operations.h"
+#import "Settings.h"
 
-#define kMaxSamples (1000)
 
 @implementation Stroke
 
 @synthesize motionManager;
 @synthesize acc;
+@synthesize direction;
 @synthesize period;
 @synthesize delegate;
 @synthesize strokes;
@@ -31,19 +33,23 @@
     [enc encodeInt:strokes forKey:@"strokes"];
 }
 
+// When initWithCoder, this object should not be used with live recordings.  See boolean "live". 
 -(id)initWithCoder:(NSCoder *)dec {
     self = [super init]; 
     if (self) {
+        live = NO;
         period = [dec decodeDoubleForKey:@"period"];
         duration = [dec decodeDoubleForKey:@"duration"];
-        size = lround(duration / period);
+        blocksize = lround(kBlockSizeSeconds / period);
         strokeData = [dec decodeObjectForKey:@"strokeData"];
         acc = [dec decodeObjectForKey:@"acc"];
         ptr = [dec decodeIntForKey:@"ptr"];
         strokes = [dec decodeIntForKey:@"strokes"];
-        N = strokeData.count * size;
-        bpx = [[Filter alloc] initWithFile:@"stroke"];
+        N = strokeData.count * blocksize;
+        NSString * sFreq = [NSString stringWithFormat:@"%1.0f", 1/period];
+        bpx = [[Filter alloc] initWithFile:[NSString stringWithFormat:@"stroke.%@",sFreq]];
         bpy = [[Filter alloc] initWithFilter:bpx];
+//        for (int i=0; i<3; i++) lowp[i]=[[Filter alloc] initWithFile:@"lowp-10.100"];
     } 
     return self;
 }
@@ -53,30 +59,40 @@
     if (self) {
         // Initialization code here.
         // acceleration
-        motionManager = [[CMMotionManager alloc] init];
-        [motionManager startDeviceMotionUpdates];
+        live = YES;
+        period = p; // effective sampling period
+        duration = d; // size of the auto-direction buffer, in seconds. 
 
-        period = p;
-        duration = d;
-        size = lround(duration / period);
-        if (N>kMaxSamples) {
-            NSLog(@"More than %d samples needed", kMaxSamples);
-            return nil;
-        }
-        acc = [[Matrix alloc] init:size by:4]; // x, y, z, t
-        strokeData = [NSMutableArray arrayWithCapacity:1000]; // few hours of stroke data...
+        motionManager = [[CMMotionManager alloc] init];
+        [motionManager startAccelerometerUpdates];
+
+        covbuffersize = lround(duration / period);
+        blocksize = lround(kBlockSizeSeconds / period); // Basic unit of accelleration storage, 100s
+        acc = [[Matrix alloc] init:blocksize by:4]; // storage of acceleration information x, y, z, t
+        strokeData = [NSMutableArray arrayWithCapacity:kBlockArrayStartsize]; // few hours of stroke data...
         ptr = 0; // next place to be filled
         N = 0; // total number of samples in buffer
         sign = 1;
-        bpx = [[Filter alloc] initWithFile:@"stroke"];
+        NSString * sFreq = [NSString stringWithFormat:@"%1.0f", 1/period];
+        bpx = [[Filter alloc] initWithFile:[NSString stringWithFormat:@"stroke.%@",sFreq]];
         bpy = [[Filter alloc] initWithFilter:bpx];
+        ma = [[VectorMA alloc] initWithLength:3 capacity:covbuffersize]; // x, y, z
         strokes = 0;
+        direction= [[Vector alloc] initWithLength:3];
+        [direction setValue:1 atIndex:1]; // y direction
 #if TARGET_IPHONE_SIMULATOR
         motionTimer = [NSTimer scheduledTimerWithTimeInterval:period target:self selector:@selector(inspectMotionSimulator:) userInfo:nil repeats:YES];
 		NSString * path = [[NSBundle mainBundle] pathForResource:@"yacc" ofType:@"10Hz"];
 		file = fopen([path UTF8String], "rt");
 #else
-        motionTimer = [NSTimer scheduledTimerWithTimeInterval:period target:self selector:@selector(inspectMotion:) userInfo:nil repeats:YES];
+        // this initializes the filters etc...
+        NSString * lowpFileName = [NSString stringWithFormat:@"lowp-%@.%1.0f",sFreq, kHardwareSamplingRate];
+//        NSLog(@"%@", lowpFileName);
+        for (int i=0; i<3; i++) lowp[i] = [[Filter alloc] initWithFile:lowpFileName];
+        [self hundredHzSampling:Settings.sharedInstance.hundredHzSampling];
+        autoOrientation = Settings.sharedInstance.autoOrientation;
+        [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(hardwareSamplingRateChanged:) name:@"hardwareSamplingRateChanged" object:nil];
+        [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(autoOrientationChanged:) name:@"autoOrientationChanged" object:nil];
 #endif
     }
     
@@ -86,7 +102,21 @@
 -(void)add:(CMAcceleration)a {
 //    double xacc = [bpx sample:a.x];
     CFAbsoluteTime time = CFAbsoluteTimeGetCurrent();
-    double yacc = [bpy sample:a.y];
+    double yacc;
+    if (autoOrientation) {
+        static int NN=1;
+        Vector * curAcc = [[Vector alloc] initWithLength:3];
+        [curAcc setValue:a.x atIndex:0];
+        [curAcc setValue:a.y atIndex:1];
+        [curAcc setValue:a.z atIndex:2];
+        [ma add:curAcc];
+        if (++NN % 10 == 0) {
+            Matrix * c = [ma cov];
+            self.direction = [c biggestEig:direction tolerance:1e-8];
+        }
+        yacc = [bpy sample:[direction inner:curAcc]];
+    } else 
+        yacc = [bpy sample:a.y];
     if (yacc * sign < 0 && fabs(yacc)>threshold) {
         sign = (yacc>0) ? 1 : -1;
         if (sign>0) { // count positive accelerations...
@@ -100,23 +130,38 @@
     [acc setValue:a.z atRow:ptr atCol:2];
     [acc setValue:time atRow:ptr atCol:3];
     ptr++;
-    if (ptr>=size) {
+    if (ptr>=blocksize) {
         ptr = 0;
         [strokeData addObject:acc];
-        self.acc = [[Matrix alloc] init:size by:3]; // should release old structure
+        self.acc = [[Matrix alloc] init:blocksize by:4]; // should release old structure
     }
     N++;
 }
 
 -(void)inspectMotion:(id)sender {
-    [self add:motionManager.deviceMotion.userAcceleration];
+    static CFTimeInterval phase = 0;
+    // This does not work for devices without gyro:
+//    [self add:motionManager.deviceMotion.userAcceleration];
+    CMAcceleration a = motionManager.accelerometerData.acceleration;
+    if (downsampling) {
+        a.x = [lowp[0] sample:a.x];
+        a.y = [lowp[1] sample:a.y];
+        a.z = [lowp[2] sample:a.z];
+        phase += hwperiod;
+        if (phase >= period) {
+            [self add:a];
+            phase -= period;
+        }
+    } else {
+        [self add:a];
+    }
 }
 
 -(void)inspectMotionSimulator:(id)sender {
     double yacc;
     fscanf(file, "%lf", &yacc);
-    CMAcceleration acc = {0,yacc,0};    
-    [self add:acc];
+    CMAcceleration accel = {(double)random()/RAND_MAX / 10, yacc, (double)random()/RAND_MAX / 10};    
+    [self add:accel];
 }
 
 -(void)startRecording {
@@ -133,12 +178,30 @@
     N = 0;
     [strokeData removeAllObjects];
 }
+
 -(void)setSensitivity:(float)logSens {
     threshold = strokeSensitivity(logSens);
 }
 
+// this set the hardware sampling rate, and initializes the downsampling
+-(void)hundredHzSampling:(BOOL)yes {
+    hardwareSamplingRate = yes ? kHardwareSamplingRate : 1/period; // valid values 10, 100
+    hwperiod = 1/hardwareSamplingRate;
+    downsampling = hardwareSamplingRate * period > 1.1;
+    if (motionTimer!=nil) [motionTimer invalidate];
+    motionTimer = [NSTimer scheduledTimerWithTimeInterval:hwperiod target:self selector:@selector(inspectMotion:) userInfo:nil repeats:YES];
+}
+
+-(void)hardwareSamplingRateChanged:(id)sender {
+    [self hundredHzSampling:Settings.sharedInstance.hundredHzSampling];
+}
+
+-(void)autoOrientationChanged:(id)sender {
+    autoOrientation = Settings.sharedInstance.autoOrientation;
+}
+
 -(Vector*)accData:(NSInteger)dim {
-    int nx = ptr + size*strokeData.count;
+    int nx = ptr + blocksize*strokeData.count;
     Vector * ret = [[Vector alloc] initWithLength:nx];
     real_t * x = ret.x;
     for (Matrix * m in strokeData)
