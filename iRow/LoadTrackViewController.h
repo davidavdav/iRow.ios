0ad30bde6d00903eacf4f72b3af84544a7d69926
David A. van Leeuwen <david.vanleeuwen@gmail.com>
2012-09-17 20:47:42 +0200

    - a browser looking for iRowTrack files.

---
+++ iRow/LoadTrackViewController.h
@@ -0,0 +1,16 @@
+//
+//  LoadTrackViewController.h
+//  iRow
+//
+//  Created by David van Leeuwen on 9/6/12.
+//  Copyright (c) 2012 strApps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LoadTrackViewController : UITableViewController {
+    NSURL * dir;
+    NSArray * tracks;
+}
+
+@end
---
+++ iRow/LoadTrackViewController.m
@@ -0,0 +1,164 @@
+//
+//  LoadTrackViewController.m
+//  iRow
+//
+//  Created by David van Leeuwen on 9/6/12.
+//  Copyright (c) 2012 strApps. All rights reserved.
+//
+
+#import "LoadTrackViewController.h"
+#import "Settings.h"
+#import "TrackExport.h"
+#import "Track+Import.h"
+
+@implementation LoadTrackViewController
+
+- (id)initWithStyle:(UITableViewStyle)style
+{
+    self = [super initWithStyle:style];
+    if (self) {
+        // Custom initialization
+        dir = [[[NSFileManager defaultManager] URLsForDirectory:NSDocumentDirectory inDomains:NSUserDomainMask] lastObject];
+        NSError * error;
+        NSPredicate * predicate = [NSPredicate predicateWithFormat:@"SELF MATCHES '.*\\.iRowTrack'"];
+        tracks = [[[NSFileManager defaultManager] contentsOfDirectoryAtPath:[dir path] error:&error] filteredArrayUsingPredicate:predicate];
+        NSLog(@"%@", tracks);
+    }
+    return self;
+}
+
+- (void)didReceiveMemoryWarning
+{
+    // Releases the view if it doesn't have a superview.
+    [super didReceiveMemoryWarning];
+    
+    // Release any cached data, images, etc that aren't in use.
+}
+
+#pragma mark - View lifecycle
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+
+    // Uncomment the following line to preserve selection between presentations.
+    // self.clearsSelectionOnViewWillAppear = NO;
+ 
+    // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+    // self.navigationItem.rightBarButtonItem = self.editButtonItem;
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+    // e.g. self.myOutlet = nil;
+}
+
+- (void)viewWillAppear:(BOOL)animated
+{
+    [super viewWillAppear:animated];
+}
+
+- (void)viewDidAppear:(BOOL)animated
+{
+    [super viewDidAppear:animated];
+}
+
+- (void)viewWillDisappear:(BOOL)animated
+{
+    [super viewWillDisappear:animated];
+}
+
+- (void)viewDidDisappear:(BOOL)animated
+{
+    [super viewDidDisappear:animated];
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    // Return YES for supported orientations
+    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+}
+
+#pragma mark - Table view data source
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+    // Return the number of sections.
+    return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    // Return the number of rows in the section.
+    return tracks.count;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    static NSString *CellIdentifier = @"Cell";
+    
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier];
+    if (cell == nil) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:CellIdentifier];
+    }
+    
+    cell.textLabel.text = [[[tracks objectAtIndex:indexPath.row] lastPathComponent] stringByDeletingPathExtension];
+    
+    return cell;
+}
+
+/*
+// Override to support conditional editing of the table view.
+- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    // Return NO if you do not want the specified item to be editable.
+    return YES;
+}
+*/
+
+/*
+// Override to support editing the table view.
+- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    if (editingStyle == UITableViewCellEditingStyleDelete) {
+        // Delete the row from the data source
+        [tableView deleteRowsAtIndexPaths:[NSArray arrayWithObject:indexPath] withRowAnimation:UITableViewRowAnimationFade];
+    }   
+    else if (editingStyle == UITableViewCellEditingStyleInsert) {
+        // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+    }   
+}
+*/
+
+/*
+// Override to support rearranging the table view.
+- (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath
+{
+}
+*/
+
+/*
+// Override to support conditional rearranging of the table view.
+- (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    // Return NO if you do not want the item to be re-orderable.
+    return YES;
+}
+*/
+
+#pragma mark - Table view delegate
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    NSString * path = [[dir URLByAppendingPathComponent:[tracks objectAtIndex:indexPath.row]] path];
+    TrackExport * export = [NSKeyedUnarchiver unarchiveObjectWithFile:path];
+    Track * new = (Track*)[NSEntityDescription insertNewObjectForEntityForName:@"Track" inManagedObjectContext:Settings.sharedInstance.moc];
+    [new importFrom:export];
+    NSError * error;
+    [Settings.sharedInstance.moc save:&error];
+    [self.navigationController popViewControllerAnimated:YES];
+}
+
+@end
